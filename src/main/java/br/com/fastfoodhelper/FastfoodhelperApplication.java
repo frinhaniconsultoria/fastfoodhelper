5f9a49f286d865ba1f9c313c3593bd3fce560624
Thiago Frinhani <frinhaniconsultoria@gmail.com>
2020-04-07 20:18:09 -0300

    Project Clean

---
+++ src/main/java/br/com/fastfoodhelper/FastfoodhelperApplication.java
@@ -0,0 +1,13 @@
+package br.com.fastfoodhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastfoodhelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastfoodhelperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fastfoodhelper/FastfoodhelperApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fastfoodhelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastfoodhelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
